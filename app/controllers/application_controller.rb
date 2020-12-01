fdf42b4e0f507fc9b0aa155106a92fb40e87329f
alisonluisk <alisonluisk@gmail.com>
2020-11-30 21:29:07 -0300

    Alterando autenticacao

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::API
 
     # code = (Time.now.year.eql? 2021) ?
     #            time.strftime("%HiBolsa2021" + request.headers['cnpj'] + "%mSA%-d") :
-    code = time.strftime("%HiBolsa%Y" + request.headers['cnpj'] + "%mSA%-d")
+    code = time.strftime("iBolsa%Y" + request.headers['cnpj'] + "%mSA%-d")
 
     request.headers['Authorization'] == Digest::SHA256.hexdigest(code)
   end
